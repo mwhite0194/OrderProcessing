ee3adb173e7a97c15b685590e6ea7fba2f25a768
John Barclay Walsh <barclay@barclaywalsh.com>
2016-01-14 21:21:14 -0500

    Added initial classes and put them into a package

---
+++ src/orderProcessing/InventoryItem.java
@@ -0,0 +1,13 @@
+/*
+ * Order Processing App – IST 411
+ */
+
+package orderProcessing;
+
+/**
+ *
+ * @author J. Barclay Walsh and Matt White
+ */
+public class InventoryItem {
+    
+}
---
+++ src/orderProcessing/TransactionMaker.java
@@ -0,0 +1,18 @@
+/*
+ * Order Processing App – IST 411
+ */
+
+package orderProcessing;
+
+/**
+ *
+ * @author J. Barclay Walsh and Matt White
+ */
+public class TransactionMaker {
+    
+    public static void main(String[] args) {
+        // Test the order processing system
+        System.out.println("Test");
+    }
+    
+}
