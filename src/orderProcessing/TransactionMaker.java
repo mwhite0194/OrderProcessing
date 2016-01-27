3ca402d0db64675bf06ad8e174236e2ed8d7824a
mpw5216 <mpw5216@202-NWD41.up.ist.local>
2016-01-26 19:25:36 -0500

    Fixed the ability for user to purchase remaining quantity

--- src/orderProcessing/TransactionMaker.java
+++ src/orderProcessing/TransactionMaker.java
@@ -16,7 +16,7 @@ public class TransactionMaker {
     public static void main(String[] args) {
         // Test the order processing system
         Scanner scanner = new Scanner(System.in);
-        boolean userIsActive = true;
+        boolean purchaseRemaining = false;
         
         // Print initial inventory
         Inventory.getInventory().printInventoryWithInventoryValue();
@@ -37,21 +37,10 @@ public class TransactionMaker {
         System.out.print("Would you like to [b]uy, [r]eturn, or [e]xchange?: ");
         String userOption = scanner.next();
         
-        //while (userIsActive == true) {
             if (userOption.startsWith("b")) {
                 Sale theSale = new Sale(0, 45, CustomerList.getCustomers().getCustomerByID(customer.getCustomerID()));
                 if (theSale.equals(false)) {
-                    // Offer to allow sale of entire current stock instead
-                    String purchaseRemaining = scanner.next();
-                    if (purchaseRemaining.startsWith("y")) {
-                        userIsActive = false; 
-                        Sale theSale2 = new Sale(0, (Inventory.getInventory().getItemByID(0).getQuantity()),
-                                CustomerList.getCustomers().getCustomerByID(customer.getCustomerID()));
-                    } else {
-                        userIsActive = false;
-                        return;
-                    }    
-                    //break;
+                   purchaseRemaining = true;
                 }
             } else if (userOption.startsWith("r")) {
                 /*if (CustomerList.getCustomers().getCustomerByID(customer.getCustomerID()).validateTransactionByID(0)) {
@@ -60,22 +49,24 @@ public class TransactionMaker {
                     System.out.println("Invalid");
                 }*/
                 Return theReturn = new Return(1, 4, CustomerList.getCustomers().getCustomerByID(customer.getCustomerID())); // (orderID, quantity, customer)
-                userIsActive = false;
             } else if (userOption.startsWith("e")) {
                 //Exchange exchange = new Exchange()
                 System.out.println("Will be added soon!");
-                userIsActive = false;
             }
-        //}
-        
-        // Print Reciept
-        //System.out.println("Customer Information: " + CustomerList.getCustomers().getCustomerByID(customer.getCustomerID()).getFullName() + " (" + CustomerList.getCustomers().getCustomerByID(customer.getCustomerID()).getFullAddress() + ")");
-        //System.out.println("Item: " + testItem.getDescription() + " - Product ID: " + testItem.getProductID() + " - Unit Price: $" + HelperMethods.priceToString(testItem.getPrice()));
-        //System.out.println("");
+            
+           if (purchaseRemaining = true) {
+            String test = scanner.next();
+            if (test.startsWith("y")) {
+                Sale theSale2 = new Sale(0, (Inventory.getInventory().getItemByID(0).getQuantity()),
+                        CustomerList.getCustomers().getCustomerByID(customer.getCustomerID()));
+            } else {
+                return;
+            }
         
         CustomerList.getCustomers().getCustomerByID(customer.getCustomerID()).printOrderHistory();
         
         Inventory.getInventory().printInventoryWithInventoryValue();
+    }
     }
     
     /**
